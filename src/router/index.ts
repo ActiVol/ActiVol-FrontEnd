80f41365732cd70c4a8aa45c314cdb2c9c579646
1834423612 <1834423612@qq.com>
2024-08-13 00:14:04 -0400

    change: make Router more easy

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,12 +4,13 @@ import Index from '../views/index.vue';
 import SubPage from '../views/subpage.vue';
 
 // Define the type of route
-interface Route {
-    path: string;
-    component: any; // Can be more strictly defined according to the actual component type
-}
+// interface Route {
+//     path: string;
+//     name?: string;
+//     component: any; // Can be more strictly defined according to the actual component type
+// }
 
-const routes: Route[] = [
+const routes = [
     {
         path: '/',
         component: Index,
