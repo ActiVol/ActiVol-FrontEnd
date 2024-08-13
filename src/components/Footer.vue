f2df4ac2b7a6a84b3e6abf48d1417323fb139753
1834423612 <1834423612@qq.com>
2024-08-12 21:58:46 -0400

    feat: add Footer on the bottom of the page

---
+++ src/components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+    <!-- Footer -->
+    <div class="text-gray-400 text-center pt-4">
+        <p>&copy; 2024 kjch</p>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'Footer',
+});
+</script>
--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -5,18 +5,25 @@
             alt="logo"
             class="w-32 h-auto rounded-lg shadow-lg mb-6"
         />
-        <div class="bg-white rounded-lg shadow-lg p-6 mx-4 max-w-md w-full">
+        <div class="bg-white rounded-lg shadow-lg p-6 mx-4 max-w-md w-fit sm:w-full">
             <h1 class="text-2xl font-bold text-center mb-4">Title</h1>
             <!-- Use the slot to insert the content -->
             <slot></slot>
         </div>
+
+        <!-- Footer -->
+        <Footer />
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import Footer from './Footer.vue';
 
 export default defineComponent({
     name: 'Layout',
+    components: {
+        Footer,
+    },
 });
 </script>
