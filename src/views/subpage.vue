8dadb19feaaa5f634bc470c24dd08f3acc22462c
1834423612 <1834423612@qq.com>
2024-08-12 21:58:18 -0400

    feat: Container boxes

--- src/views/subpage.vue
+++ src/views/subpage.vue
@@ -1,8 +1,23 @@
 <template>
     <Layout>
         <Breadcrumb :items="breadcrumbItems" currentPage="SubPage" />
-        <div class="bg-gray-200 rounded-md p-4">
+        <div class="bg-rose-100 rounded-md px-4 py-3">
             <p class="text-center text-gray-700">This is the content for the subpage.</p>
+            <!-- Boxes area -->
+            <div class="grid grid-cols-1 gap-4 mt-4">
+                <div class="bg-white rounded-lg shadow-lg p-4">
+                    <h2 class="text-lg font-bold text-gray-800">Box 1</h2>
+                    <p class="text-gray-700">This is the content for box 1.</p>
+                </div>
+                <div class="bg-white rounded-lg shadow-lg p-4">
+                    <h2 class="text-lg font-bold text-gray-800">Box 2</h2>
+                    <p class="text-gray-700">This is the content for box 2.</p>
+                </div>
+                <div class="bg-white rounded-lg shadow-lg p-4">
+                    <h2 class="text-lg font-bold text-gray-800">Box 3</h2>
+                    <p class="text-gray-700">This is the content for box 3.</p>
+                </div>
+            </div>
         </div>
     </Layout>
 </template>
