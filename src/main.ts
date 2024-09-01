8cdeca581c6dd451188f580791581be753e23a1e
kjch <1834423612@qq.com>
2024-09-01 02:14:52 +0000

    feat: add @iconify/vue in main.ts for global

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@ import App from './App.vue'
 import router from './router'
 import Swal from 'sweetalert2'
 import 'iconify-icon';
+import { Icon } from '@iconify/vue';
 
 const app = createApp(App)
 app.use(router)
 app.config.globalProperties.$swal = Swal
+app.component('icon', Icon)
 app.mount('#app')
