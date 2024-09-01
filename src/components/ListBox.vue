21c91d94dc200bca919103c9df16b33b8bcd9334
kjch <1834423612@qq.com>
2024-09-01 22:41:11 +0000

    fix: change listbox icon

--- src/components/ListBox.vue
+++ src/components/ListBox.vue
@@ -10,12 +10,13 @@
             <h2 class="text-lg font-bold text-gray-800">{{ title }}</h2>
             <p class="text-gray-700">{{ content }}</p>
         </div>
-        <iconify-icon icon="mdi:chevron-right" class="text-gray-500"></iconify-icon>
+        <Icon icon="mdi:chevron-right" class="text-gray-500" class="h-5 w-5 text-gray-400" aria-hidden="true" />
     </router-link>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { Icon } from '@iconify/vue';
 
 export default defineComponent({
     name: 'ListBox',
