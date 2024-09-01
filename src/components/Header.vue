03baa9c1b7e6d29395d9500700afc1c7403d1def
kjch <1834423612@qq.com>
2024-09-01 19:33:02 +0000

    feat: add Header.vue & click to link home

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <header class="mb-6 text-center">
+        <router-link to="/" class="bg-blue-600 text-white text-2xl font-bold py-3 px-6 rounded-lg inline-block">
+            标题/LOGO
+        </router-link>
+    </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'Header',
+});
+</script>
--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,11 +1,7 @@
 <template>
     <div class="flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-50 to-blue-100">
         <div class="flex flex-col items-center justify-between w-full max-w-5xl p-4 sm:max-lg:p-12 xl:max-2xl:p-24">
-            <header class="mb-6 text-center">
-                <div class="bg-blue-600 text-white text-2xl font-bold py-3 px-6 rounded-lg inline-block">
-                    标题/LOGO
-                </div>
-            </header>
+            <Header />
             <Breadcrumb :items="breadcrumbItems" :currentPage="currentPage" />
             <div class="bg-white rounded-lg shadow-lg flex-grow flex flex-col items-center p-6">
                 <!-- <h1 v-if="!$route.meta.hideTitle" class="text-2xl font-bold text-center mb-4">Title</h1> -->
@@ -21,6 +17,7 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import Header from './Header.vue';
 import Footer from './Footer.vue';
 import Breadcrumb from './Breadcrumb.vue';
 
@@ -32,6 +29,7 @@ interface BreadcrumbItem {
 export default defineComponent({
     name: 'Layout',
     components: {
+        Header,
         Footer,
         Breadcrumb,
     },
