13997f9864b3ff0a05e4f107c865e6c5907550be
kjch <1834423612@qq.com>
2024-09-13 21:21:24 +0000

    fix: admin panel Modal component layout

--- src/components/Admin/Modal.vue
+++ src/components/Admin/Modal.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full" id="my-modal">
-        <div class="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white">
+    <div class="flex items-center fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full" id="my-modal">
+        <div class="relative mx-4 md:mx-auto p-5 border w-96 shadow-lg rounded-md bg-white">
             <div class="mt-3 text-center">
                 <slot></slot>
             </div>
