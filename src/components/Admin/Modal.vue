77b39c0ab3abed8d77ac1d88f4be4e3e5069397e
kjch <1834423612@qq.com>
2024-09-14 01:55:41 +0000

    fix: admin panel Modal component index level layer

--- src/components/Admin/Modal.vue
+++ src/components/Admin/Modal.vue
@@ -1,7 +1,9 @@
 <template>
-    <div class="flex items-center fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full" id="my-modal">
-        <div class="relative mx-4 md:mx-auto p-5 border w-96 shadow-lg rounded-md bg-white">
-            <div class="mt-3 text-center">
+    <div class="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full z-50 flex items-center justify-center p-4 sm:p-6 md:p-8"
+        id="my-modal">
+        <div
+            class="relative bg-white rounded-lg shadow-xl w-full max-w-md sm:max-w-lg md:max-w-xl lg:max-w-2xl xl:max-w-4xl overflow-hidden">
+            <div class="max-h-[calc(100vh-2rem)] overflow-y-auto p-6">
                 <slot></slot>
             </div>
         </div>
@@ -10,7 +12,7 @@
 
 <script>
 export default {
-    name: 'Modal',
-    emits: ['close']
-}
+    name: "Modal",
+    emits: ["close"],
+};
 </script>
