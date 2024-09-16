7487ef083cd7a15f670229296f95ac92cc8e92bd
kjch <1834423612@qq.com>
2024-09-16 05:51:34 +0000

    fix: index layout component & i18n button style

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -2,7 +2,12 @@
     <div class="flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-100 to-blue-200">
         <div class="flex flex-col items-center justify-between w-full max-w-5xl p-4 sm:p-6 lg:p-8">
             <Header />
-            <Breadcrumb :items="breadcrumbItems" :currentPage="currentPage" />
+            <div class="flex justify-between w-full items-stretch">
+                <div class="items-center">
+                <Breadcrumb :items="breadcrumbItems" :currentPage="currentPage" class="" />
+            </div>
+                <LanguageSelector />
+            </div>
             <div class="bg-white rounded-lg shadow-lg w-full flex flex-col items-center p-4 sm:p-6">
                 <slot></slot>
             </div>
@@ -17,6 +22,7 @@ import Header from './Header.vue';
 import Footer from './Footer.vue';
 import Breadcrumb from './Breadcrumb.vue';
 import { BreadcrumbItem } from '../types'; // 确保导入接口
+import LanguageSelector from '@/components/LanguageSelector.vue';
 
 export default defineComponent({
     name: 'Layout',
@@ -24,6 +30,7 @@ export default defineComponent({
         Header,
         Footer,
         Breadcrumb,
+        LanguageSelector,
     },
     props: {
         breadcrumbItems: {
