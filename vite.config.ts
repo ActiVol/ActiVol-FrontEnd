06afa1b92d1b3437add5ac7572cbb95d408e2e1f
kjch <1834423612@qq.com>
2024-10-13 18:16:15 +0000

    feat: improve for build options

--- vite.config.ts
+++ vite.config.ts
@@ -14,5 +14,17 @@ export default defineConfig({
     open: false, // Automatically open the browser
     cors: true, // Allow cross-origin resource sharing
     host: '0.0.0.0', // Allow external access
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000 // 调整块大小警告限制
   }
 })
